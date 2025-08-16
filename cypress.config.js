ff9d0938f94357c8a8dee61165a533c777f97fb1
Пользователь <augul.evgeniy@gmail.com>
2025-08-16 23:42:29 +0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+        on('task', {
+        logCatch(message) {
+          console.log(message);
+          return null;
+        }
+      });
     },
   testIsolation: false,
   },
